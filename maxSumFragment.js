4e55fa0f4b499cd74539821b393e2ebde0c59ca5
Ivan Dashkevich <idashkevich@lement.pro>
2018-11-05 14:27:47 +0300

    Задача 12: Найти фрагмент с наибольшей суммой

--- maxSumFragment.js
+++ maxSumFragment.js
@@ -5,5 +5,23 @@ const maxSumFragment = (str) => {
     if (initialArray.includes("")) return;
     initialArray = initialArray.map((num) => { return parseInt(num, 10) });
 
-    console.log(initialArray);
+    const fragmentSize = 3;
+    if (initialArray.length - fragmentSize < 0) {
+        console.log(`Необходим массив минимум из ${fragmentSize} элементов`);
+        return;
+    }
+
+    const sums = [];
+    for (let i = 0; i < initialArray.length - fragmentSize + 1; i++) {
+        let sum = 0;
+        for (let j = i; j < i + fragmentSize; j++) {
+            sum += initialArray[j];
+        }
+        sums.push(sum);
+    }
+
+    const max = Math.max.apply(null, sums);
+    const maxIndex = sums.indexOf(max);
+
+    console.log(`Фрагмент из ${fragmentSize} элементов с максимальной суммой ${max} начинается с индекса ${maxIndex}`);
 }
